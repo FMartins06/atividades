c2555a8ad6009c2f2b2d1ee24682a76f576f9643
FMartins06 <joaofvmr@gmail.com>
2025-09-09 20:39:10 -0300

    Add files via upload

---
+++ print_APC.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+printf("    _    ____   ____\n");
+printf("   / \\  |  _ \\ / ___|\n");
+printf("  / _ \\ | |_) | |    \n");
+printf(" / ___ \\|  __/| |___ \n");
+printf("/_/   \\_\\_|    \\____|\n");
+}
